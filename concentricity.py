db7b9a0659974f96dfadce8d736cd1a9d6d2d6de
Anshul Gupta <ansg191@yahoo.com>
2020-07-21 14:16:56 -0700

    Make into module and document

--- concentricity.py
+++ concentricity.py
@@ -1,3 +1,64 @@
+"""This python script detects the outer most circle and gives its center to compare it to the emitter.
+
+Author: Anshul Gupta
+
+Usage:
+As a script:
+    python concentricity.py [-h] [-c CONFIG] [-s] [--show-extra] [-v] [--show-ignored] [-p] FILE
+
+    positional arguments:
+        FILE                        file to analyze
+
+    optional arguments:
+        -h, --help                  show help message
+        -c CONFIG, --config CONFIG  provide config file
+        -s, --show                  Show main openCV windows
+        --show-extra                Show debug openCV windows. This will also enable -s flag
+        -v, --verbose               Show debug log. This will show some of the inner workings
+        --show-ignored              Show ignored warnings. Enabling this will show the suppressed runtime warnings
+        -p, --show-plots            Show plots of curve fitting. Use this to debug poor curve fits
+
+As a module:
+    >>> import concentricity as con
+    >>> import cv2
+
+    Config file must be provided before module can be used
+    >>> con.process_config('config.ini')
+
+    Use process_image function to get center and radius
+    >>> image = cv2.imread('image.png')
+    >>> con.process_image(image)
+
+    If save folder is defined in config file, files will be stored in 'save_folder/import'
+
+Config file (Options with values are recommended values):
+    [CROP]                          Options for cropping image
+    x1 =                            Left x position, leave x1 blank for no crop and to ignore rest of CROP options
+    x2 =                            Right x position
+    y1 =                            Top y position
+    y2 =                            Bottom y position
+
+    [EDGE DETECTION]                Options for Edge Detection
+    filter = 30                     Diameter of pixel neighborhood for bilateral filter, std of color space is filter*2,
+                                    std of coord space is filter / 2
+    edge low = 150                  Canny Edge Detection minVal
+    edge high = 200                 Canny Edge Detection maxVal
+
+    [CIRCLE FIT]                    Options for Fitting the Circle
+    high cutoff = 50                Starting Cutoff residual. Lower values will aggressively cutoff more values.
+                                    Higher values may require more runs to remove outliers
+    low cutoff = 2.5                Final Cutoff residual. Lower values may cutoff good values. Higher values may
+                                    leave outliers in final fit
+    runs = 10                       Number of residual cuts and refits. More runs increase processing time
+    method = 3                      Method for determining final circle. 3 uses a circumcircle, which produces better
+                                    results. 4 uses cardinal points which may be more inaccurate
+
+    [OUTPUT]                        Options for Output images
+    zoom size = 100                 Size to zoom into. Zoom will be a square in the center of the picture
+    save folder =                   Folder to save files to. Put as blank for no output
+    save type = png                 Type to save images as
+"""
+
 import argparse
 import configparser
 import logging
@@ -13,13 +74,18 @@ from scipy import optimize
 parser = argparse.ArgumentParser(description="Checks if emitter is located at the center of the outer circle")
 parser.add_argument('file', help='File to analyze')
 parser.add_argument('-c', '--config', default='concentricity.ini', help='Config file')
-parser.add_argument('-s', '--show', action='store_true', help='Show openCV windows')
-parser.add_argument('--show-extra', action='store_true', help='Show debug openCV windows')
+parser.add_argument('-s', '--show', action='store_true', help='Show main openCV windows')
+parser.add_argument('--show-extra', action='store_true', help='Show debug openCV windows. -s will be enabled')
 parser.add_argument('-v', '--verbose', action='store_true', help='Show debug log')
 parser.add_argument('--show-ignored', action='store_true', help='Show ignored warnings')
 parser.add_argument('-p', '--show-plots', action='store_true', help='Show plots of curve fitting')
 
-args = parser.parse_args()
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if not os.path.isfile(args.config):
+        raise FileNotFoundError(f'{args.config} could not be found')
+else:
+    args = parser.parse_args(["import", '-c', 'none'])
 
 # if showing debug, the regular ones should be shown too
 if args.show_extra:
@@ -35,30 +101,52 @@ if args.verbose:
 else:
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-# Read config file
-config = configparser.ConfigParser()
-config.read(args.config)
+# Setup config variables
+x1, x2, y1, y2, filter_val, edge_low, edge_high, \
+high_cutoff, low_cutoff, runs, method, zoom_size, save_folder, save_type = [None] * 14
 
-try:
-    x1 = config.getint('CROP', 'x1')
-except ValueError:
-    x1 = None
-x2 = config.getint('CROP', 'x2')
-y1 = config.getint('CROP', 'y1')
-y2 = config.getint('CROP', 'y2')
+file_name = 'import'
 
-filter_val = config.getint('EDGE DETECTION', 'filter')
-edge_low = config.getint('EDGE DETECTION', 'edge low')
-edge_high = config.getint('EDGE DETECTION', 'edge high')
 
-high_cutoff = config.getfloat('CIRCLE FIT', 'high cutoff')
-low_cutoff = config.getfloat('CIRCLE FIT', 'low cutoff')
-runs = config.getint('CIRCLE FIT', 'runs')
-method = config.getint('CIRCLE FIT', 'method')
+def process_config(file):
+    """Initialize parameters using config file
 
-zoom_size = config.getint('OUTPUT', 'zoom size')
-save_folder = config.get('OUTPUT', 'save folder')
-save_type = config.get('OUTPUT', 'save type')
+    :param file: config file
+    :type file: str
+    """
+    global x1, x2, y1, y2, filter_val, edge_low, edge_high, high_cutoff, low_cutoff, runs, method, zoom_size, \
+        save_folder, save_type
+    # Read config file
+    config = configparser.ConfigParser()
+    config.read(file)
+
+    try:
+        x1 = config.getint('CROP', 'x1')
+    except ValueError:
+        x1 = None
+    else:
+        x2 = config.getint('CROP', 'x2')
+        y1 = config.getint('CROP', 'y1')
+        y2 = config.getint('CROP', 'y2')
+
+    filter_val = config.getint('EDGE DETECTION', 'filter')
+    edge_low = config.getint('EDGE DETECTION', 'edge low')
+    edge_high = config.getint('EDGE DETECTION', 'edge high')
+
+    high_cutoff = config.getfloat('CIRCLE FIT', 'high cutoff')
+    low_cutoff = config.getfloat('CIRCLE FIT', 'low cutoff')
+    runs = config.getint('CIRCLE FIT', 'runs')
+    method = config.getint('CIRCLE FIT', 'method')
+
+    zoom_size = config.getint('OUTPUT', 'zoom size')
+    save_folder = config.get('OUTPUT', 'save folder')
+    save_type = config.get('OUTPUT', 'save type')
+
+    if save_folder:
+        try:
+            os.mkdir(os.path.join(save_folder, 'import'))  # Create output folder
+        except FileExistsError:
+            pass
 
 
 def calc_R(x, y, xc, yc):
@@ -106,7 +194,7 @@ def fit(x, y, center):
     :return: Tuple of residuals, center, and radius
     :rtype: (np.ndarray, (float, float), float)
     """
-    # noinspection PyTupleAssignmentBalance
+    # noinspection PyTupleAssignmentBalance,PyTypeChecker
     center, _ = optimize.leastsq(f, center, args=(x, y))
     ri = calc_R(x, y, *center)
     r = ri.mean()
@@ -267,6 +355,14 @@ def process_image(img):
     :param img: Image in BGR format
     :type img: np.ndarray
     """
+    if method is None:
+        raise RuntimeError("Config has not been initialized. Initialize using process_config")
+
+    if x1:
+        img = img[y1:y2, x1:x2]  # Crop image
+        if args.show_extra:
+            cv2.imshow('Crop', img)
+
     center, radius = get_center(img)  # Get center and radius
     icenter = tuple(np.round(center).astype(int))  # Convert to integer for openCV
 
@@ -318,26 +414,26 @@ def process_image(img):
             logging.debug(f'Wrote zoom image to {save_file}')
 
 
-image = cv2.imread(args.file)  # Read image
-if args.show_extra:
-    cv2.imshow('Image', image)
-
-if save_folder:
-    file_name = os.path.split(args.file)[-1]
+if args.file != 'import':
     try:
-        os.mkdir(os.path.join(save_folder, file_name))  # Create output folder
-    except FileExistsError:
-        pass
-
-if x1:
-    image = image[y1:y2, x1:x2]  # Crop image
+        process_config(args.config)
+    except configparser.NoSectionError:
+        logging.error('Config file failed to parse')
+    image = cv2.imread(args.file)  # Read image
     if args.show_extra:
-        cv2.imshow('Crop', image)
+        cv2.imshow('Image', image)
 
-process_image(image)  # Process image
+    if save_folder:
+        file_name = os.path.split(args.file)[-1]
+        try:
+            os.mkdir(os.path.join(save_folder, file_name))  # Create output folder
+        except FileExistsError:
+            pass
 
-if args.show_plots:
-    plt.show()
-if args.show:
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    process_image(image)  # Process image
+
+    if args.show_plots:
+        plt.show()
+    if args.show:
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
